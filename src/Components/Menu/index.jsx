c05e26c20f450e5a4ed14cfba7fe934e2c50103d
Piter <piotr.podyma10@gmail.com>
2019-07-07 19:26:27 +0200

    Setup project

---
+++ src/Components/Menu/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.scss';
+
+const Manu = () => {
+  return (
+    <div className="App">
+      Sample text 2 
+    </div>
+  );
+}
+
+export default Manu;
