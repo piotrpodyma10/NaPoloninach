a9b8a1f9dbe40907775e288e2cb138c12d091d3e
Piter <piotr.podyma10@gmail.com>
2019-07-23 21:53:05 +0200

    Added small changes

--- src/Views/Home/index.jsx
+++ src/Views/Home/index.jsx
@@ -5,15 +5,15 @@ import Menu from '../../Components/Menu'
 const Home = () => {
   return (
     <Fragment>
-      <div className="HomeContainer">
+      <div className="PageContainer">
         <Menu />
-      </div>
+      <div className="HomeContainer"></div>
       <div style={{height: '70px', width: '100%'}}></div>
+      <div className="SecondPage"></div>
 
-      <div className="SecondPage">
-      </div>
       <div style={{height: '70px', width: '100%'}}></div>
       <div className="ThirdPage"></div>
+      </div>
     </Fragment>
   );
 }
